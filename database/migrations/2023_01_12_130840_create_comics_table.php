20dc75de201b7da439ec57dd3229ec0bb1226967
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2023-01-12 15:27:42 +0100

    popolato il seeder

--- database/migrations/2023_01_12_130840_create_comics_table.php
+++ database/migrations/2023_01_12_130840_create_comics_table.php
@@ -16,10 +16,10 @@ return new class extends Migration
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
             $table->string('title', 150);
-            $table->string('slug', 110)->unique();
+            $table->string('slug', 200)->unique();
             $table->text('description');
             $table->string('thumb');
-            $table->float('price', 6, 2);
+            $table->string('price', 10);
             $table->string('series');
             $table->date('sale_date');
             $table->string('type');
--- database/seeders/ComicsTableSeeder.php
+++ database/seeders/ComicsTableSeeder.php
@@ -18,13 +18,15 @@ class ComicsTableSeeder extends Seeder
         $array_comics = config('db.comics');
         foreach ($array_comics as $comic_item) {
             $new_comic = new Comic();
-            $new_comic->title = $comic_item->title;
-            $new_comic->text = $comic_item->description;
-            $new_comic->string = $comic_item->thumb;
-            $new_comic->float = $comic_item->price;
-            $new_comic->string = $comic_item->series;
-            $new_comic->date = $comic_item->sale_date;
-            $new_comic->string = $comic_item->type;
+            $new_comic->title = $comic_item['title'];
+            $new_comic->slug = Comic::generateSlug($new_comic->title);
+            $new_comic->description = $comic_item['description'];
+            $new_comic->thumb = $comic_item['thumb'];
+            $new_comic->price = $comic_item['price'];
+            $new_comic->series = $comic_item['series'];
+            $new_comic->sale_date = $comic_item['sale_date'];
+            $new_comic->type = $comic_item['type'];
+            $new_comic->save();
         }
     }
 }
